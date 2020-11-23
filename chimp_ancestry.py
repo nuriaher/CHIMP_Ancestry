763ffb3db22ae80665976a23948ae056b6d491f3
nuriaher <nuria.albala@sund.ku.dk>
2020-11-23 17:08:41 +0100

    upd

---
+++ chimp_ancestry.py
@@ -0,0 +1,26 @@
+#Núria Hermosilla-Albala
+#20.11.20
+
+##########################################################
+############# CHIMP Ancestry Pipeline EEP ################
+##########################################################
+
+#a = system("echo This_is_the_output", intern = TRUE)
+
+
+
+######
+### 1 - ADMIXTURE & evalADMIX on Reference Panel
+###
+
+
+
+######
+### 2 - Relatedness check INDV vs Reference Panel
+###
+
+
+
+######
+### 1 - ADMIXTURE & evalADMIX on Reference Panel
+###
